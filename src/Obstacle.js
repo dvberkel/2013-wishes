8870437bca60c8dd10c1623557524ad2ee6dceb2
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-07-22 12:53:08 +0200

    [REFACTOR] obstacle introduced
    An obstacle is introduced which serves as a superclass for other obstacles.

--- src/Obstacle.js
+++ src/Obstacle.js
@@ -1,17 +1,11 @@
 (function(_, Backbone, Wish, undefined){
-    var Wall = Backbone.Model.extend({
-	defaults : { x: 0 },
-
+    var Obstacle = Backbone.Model.extend({
 	observe : function(aBall){
 	    aBall.bind("change:position", function(ball){
 		if (this.isHitBy(ball)) {
-		    ball.reflectVx();
+		    this.change(ball);
 		}
 	    }, this);
-	},
-
-	isHitBy : function(aBall) {
-	    return this.isHitFromLeftBy(aBall) || this.isHitFromRightBy(aBall);
 	},
 
 	isHitFromLeftBy : function(aBall) {
@@ -22,30 +16,6 @@
 	    return aBall.isHeadingLeft() && this.isInRangeOf(aBall);
 	},
 
-	isInRangeOf : function(aBall) {
-	    var position = aBall.get("position");
-	    var velocity = aBall.get("velocity");
-	    return Math.abs(position.x - this.get("x")) <= Math.abs(velocity.vx);
-	}
-
-    });
-
-    var Ceiling = Backbone.Model.extend({
-	defaults : { y : 0 },
-
-	observe : function(aBall){
-	    aBall.bind("change:position", function(ball){
-		if (this.isHitBy(ball)) {
-		    ball.reflectVy();
-		}
-	    }, this);
-	    return this;
-	},
-
-	isHitBy : function(aBall) {
-	    return this.isHitFromBelowBy(aBall) || this.isHitFromAboveBy(aBall);
-	},
-
 	isHitFromBelowBy : function(aBall) {
 	    return aBall.isHeadingUp() && this.isInRangeOf(aBall);
 	},
@@ -54,11 +24,50 @@
 	    return aBall.isHeadingDown() && this.isInRangeOf(aBall);
 	},
 
-	isInRangeOf : function(aBall) {
+	isInHorizontalRangeOf : function(aBall) {
+	    var position = aBall.get("position");
+	    var velocity = aBall.get("velocity");
+	    return Math.abs(position.x - this.get("x")) <= Math.abs(velocity.vx);
+	},
+
+	isInVerticalRange : function(aBall) {
 	    var position = aBall.get("position");
 	    var velocity = aBall.get("velocity");
 	    return Math.abs(position.y - this.get("y")) <= Math.abs(velocity.vy);
+	},
+    });
+
+    var Wall = Obstacle.extend({
+	defaults : { x: 0 },
+
+
+	isHitBy : function(aBall) {
+	    return this.isHitFromLeftBy(aBall) || this.isHitFromRightBy(aBall);
+	},
+
+	isInRangeOf : function(aBall) {
+	    return this.isInHorizontalRangeOf(aBall);
+	},
+
+	change : function(aBall) {
+	    aBall.reflectVx();
 	}	
+    });
+
+    var Ceiling = Obstacle.extend({
+	defaults : { y : 0 },
+
+	isHitBy : function(aBall) {
+	    return this.isHitFromBelowBy(aBall) || this.isHitFromAboveBy(aBall);
+	},
+
+	isInRangeOf : function(aBall) {
+	    return this.isInVerticalRange(aBall);
+	},
+
+	change : function(aBall) {
+	    aBall.reflectVy();
+	}
     });
 
     Wish.Wall = Wall;
