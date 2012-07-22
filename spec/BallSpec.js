da6286be38da5350e131b7966179bbd85cb06632
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-07-22 11:35:13 +0200

    Ball Updte
    The update function is created.

--- spec/BallSpec.js
+++ spec/BallSpec.js
@@ -16,4 +16,14 @@ describe("a Ball", function(){
     it("should have a default velocity", function(){
 	expect(ball).toHaveVelocity({ vx: 1, vy: 1});
     });
+
+    it("should move on update", function(){
+	var position = ball.get("position");
+	var velocity = ball.get("velocity");
+
+	ball.update();
+
+	expect(ball).toBeAt({ x: position.x + velocity.vx, y: position.y + velocity.vy });
+	expect(ball).toHaveVelocity(velocity);
+    });
 });
--- src/Ball.js
+++ src/Ball.js
@@ -3,6 +3,16 @@
 	defaults : {
 	    "position" : { x: 0, y: 0 },
 	    "velocity" : { vx: 1, vy: 1 }
+	},
+
+	update : function(){
+	    var position = this.get("position");
+	    var velocity = this.get("velocity");
+
+	    this.set({ "position" : {
+		x: position.x + velocity.vx,
+		y: position.y + velocity.vy
+	    }});
 	}
     });
 
