0236f2a72dc49ab6b83a8ef34310a078d048bf8f
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-07-22 11:26:24 +0200

    [REFACTOR] beforeEach
    Jasmines beforeEach is used.

--- spec/BallSpec.js
+++ spec/BallSpec.js
@@ -1,17 +1,19 @@
 describe("a Ball", function(){
+    var ball;
+
+    beforeEach(function(){
+	ball = new Wish.Ball();
+    });
+
     it("should be defined", function(){
 	expect(Wish.Ball).toBeDefined();
     });
 
     it("should have a default position", function(){
-	var ball = new Wish.Ball();
-
 	expect(ball).toBeAt({ x: 0, y: 0});
     });
 
     it("should have a default velocity", function(){
-	var ball = new Wish.Ball();
-
 	expect(ball).toHaveVelocity({ vx: 1, vy: 1});
     });
 });
