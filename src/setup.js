531a73f6f1b217a65cdba388d8573caf0b8bc331
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-07-28 12:13:46 +0200

    Raphael setup
    Raphael is setup to interact with the the models (via views).

---
+++ src/setup.js
@@ -0,0 +1,10 @@
+(function($, Raphael, Wish, undefined){
+    $(function(){
+	var paper = Raphael("screen", 640, 480);
+	paper.rect(0, 0, 640, 480).attr({
+	    "fill": "#eeeeee",
+	    "stroke": "none"
+	});
+	
+    });
+})(jQuery, Raphael, Wish);
