f5fdc5f3be06fdf21c1fab502c560197729c5ddb
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-12-27 12:09:54 +0100

    obstacles should observe ball

--- src/Environment.js
+++ src/Environment.js
@@ -4,13 +4,23 @@
 	    var width = this.get("width");
 	    var height = this.get("height");
 
-	    this.set("leftWall", new Wish.Wall({ x: 0 }));
-	    this.set("rightWall", new Wish.Wall({ x: width }));
-	    this.set("ceiling", new Wish.Ceiling({ y: height }));
+	    var ball = new Wish.Ball({ "position" : { x: 50, y: 30}, "velocity" : { vx: 1, vy: 1}});
+	    var leftWall = new Wish.Wall({ x: 0 });
+	    var rightWall = new Wish.Wall({ x: width });
+	    var ceiling = new Wish.Ceiling({ y: height });
+	    var paddle = new Wish.Paddle({ "position" : { x: width/2, y: 20 }, "extend" : { "width": 40, "height": 10 }});
+	    
+	    leftWall.observe(ball);
+	    rightWall.observe(ball);
+	    ceiling.observe(ball);
+	    paddle.observe(ball);
 
-	    this.set("ball", new Wish.Ball({ "position" : { x: 50, y: 30}, "velocity" : { vx: 1, vy: 1}}));
+	    this.set("ball", ball);
 
-	    this.set("paddle",new Wish.Paddle({ "position" : { x: width/2, y: 20 }, "extend" : { "width": 40, "height": 10 }}));
+	    this.set("leftWall", leftWall);
+	    this.set("rightWall", rightWall);
+	    this.set("ceiling", ceiling);
+	    this.set("paddle", paddle);
 	},
 
 	update : function(){
