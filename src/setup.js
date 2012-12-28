4a38d4fac76c36d0830bfbfefc6d1b96b69a4815
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-12-28 11:03:01 +0100

    use wish from parameters

--- src/setup.js
+++ src/setup.js
@@ -1,4 +1,8 @@
 (function($, Raphael, Wish, undefined){
+    var parameters = requestParameters();
+    var wish = $.base64.decode(unescape(parameters.w)) || "Happy 2013!";
+    console.log(wish);
+    
     var requestAnimFrame = (function(){
 	return window.requestAnimationFrame ||
             window.webkitRequestAnimationFrame ||
@@ -22,7 +26,7 @@
 	    "horizontalBrickCount" : 10, "verticalBrickCount" : 3,
 	    "velocity" : { vx : 2.5, vy : 4 },
 	    "paddleExtend" : { width : 80, height : 10 },
-	    "wish" : "Have a wonderfull 2013!"
+	    "wish" : wish
 	});
 	new Wish.EnvironmentView({ "model" : environment, "paper" : paper });
 
